296c5304697537b26440155792669c3b275b80f1
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-08-10 03:37:03 -0400

    📐 Fixed Centering Elements

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 console.log("app.js intact");
 
-function position() {
-    //center and size game view
-    (() => {
-        let size;
-        if (height < width) {
-            size = height;
-        } else if (height > width) {
-            size = width;
-        }
-
-        game.style.width = size + "px";
-        game.style.height = size + "px";
-
-        game.style.top = ((height / 2) - (size / 2)) + "px";
-        game.style.left = ((width / 2) - (size / 2)) + "px";
-    })();
-
-    (() => {
-    })();
-}
-
 function run() {
-    position();
+    generateTileSet();
+    render();
+    console.log(data.game.world.tileset);
 }
 
 run();
